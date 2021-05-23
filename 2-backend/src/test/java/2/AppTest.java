fd85e6ff354b674d05d322a0b81bfd164429046c
root <root@ip-172-31-47-127.us-east-2.compute.internal>
2021-05-23 02:20:14 +0000

    all

---
+++ 2-backend/src/test/java/2/AppTest.java
@@ -0,0 +1,12 @@
+package 2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
